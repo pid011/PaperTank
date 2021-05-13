a408a65c27f708d471fa5d553195ccf8e30ef6af
pid011 <pid011@kakao.com>
2021-05-14 04:56:50 +0900

    Add tank

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace PaperTank
+{
+    public class PlayerMove : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private SpriteRenderer _playerSprite;
+
+        private Rigidbody _rigidbody;
+        private float _horizontal;
+        private float _vertical;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            _horizontal = Input.GetAxis("Horizontal");
+            _vertical = Input.GetAxis("Vertical");
+
+            if (_horizontal < 0) _playerSprite.flipX = true;
+            if (_horizontal > 0) _playerSprite.flipX = false;
+        }
+
+        private void FixedUpdate()
+        {
+            var horizontalMove = _horizontal * _moveSpeed;
+            var verticalMove = _vertical * _moveSpeed;
+
+            _rigidbody.velocity = new Vector3(horizontalMove, _rigidbody.velocity.y, verticalMove);
+        }
+    }
+}
---
+++ Assets/Scripts/View/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace PaperTank
+{
+    public class CameraMove : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed = 2.0f;
+        [SerializeField] private Transform _target;
+        [SerializeField] private int _height;
+        [SerializeField] private Vector2 _offset;
+
+        private void Update()
+        {
+            var move = _target.position + Vector3.up * _height + new Vector3(_offset.x, 0, _offset.y);
+            transform.position = Vector3.Lerp(transform.position, move, _moveSpeed * Time.deltaTime);
+        }
+    }
+}
